4a840d1d725517b3045c8c3c7c3be362e89ef6e4
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-07-01 21:32:37 +0530

    fea: Added  layout code to Layout Lib

--- libs/frontend/layout/src/lib/layout/layout.ts
+++ libs/frontend/layout/src/lib/layout/layout.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { BackdropComponent } from '../../components/backdrop/backdrop.component';
+import { HeaderComponent } from '../../components/header/header.component';
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
+import { SidebarService } from '../../services/sidebar/sidebar.service';
 
 @Component({
   selector: 'lib-layout',
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    HeaderComponent,
+    SidebarComponent,
+    BackdropComponent,
+  ],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
 })
-export class Layout {}
+export class Layout {
+  sidebar = inject(SidebarService);
+}
