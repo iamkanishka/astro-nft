059e7da3c030d72f3dff3e55e0491645fa557b04
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-07-01 21:23:50 +0530

    feat: Explore  UI  Library

---
+++ libs/frontend/explore/src/lib/explore/explore.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-explore',
+  imports: [CommonModule],
+  templateUrl: './explore.html',
+  styleUrl: './explore.css',
+})
+export class Explore {}
