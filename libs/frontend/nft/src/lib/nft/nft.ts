cb15e35bea8d99aa10a0e50bb5bad1005f2f8419
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-07-01 21:24:25 +0530

    feat: NFT  UI  Library

---
+++ libs/frontend/nft/src/lib/nft/nft.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-nft',
+  imports: [CommonModule],
+  templateUrl: './nft.html',
+  styleUrl: './nft.css',
+})
+export class Nft {}
