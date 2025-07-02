1869d69b531d0e5ba2904735d9c4a701d5b74263
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-07-02 21:05:13 +0530

    collections comp

---
+++ libs/frontend/dashboard/src/components/collections-section/collections-section.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-collections-section',
+  imports: [],
+  templateUrl: './collections-section.html',
+  styleUrl: './collections-section.css'
+})
+export class CollectionsSection {
+
+}
