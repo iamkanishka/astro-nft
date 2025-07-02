8c9947f39c9c9c536faa8d29ddec24b906911a54
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-07-02 21:05:53 +0530

    todays-pick comp

---
+++ libs/frontend/dashboard/src/components/todays-pick/todays-pick.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-todays-pick',
+  imports: [],
+  templateUrl: './todays-pick.html',
+  styleUrl: './todays-pick.css'
+})
+export class TodaysPick {
+
+}
