718f11e946bb17ac73b5b03ccf5680244fa7b6de
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-07-03 21:16:10 +0530

    feat: Added template

---
+++ libs/frontend/dashboard/src/lib/components/features-section/features-section.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-dashboard-features-section',
+  imports: [],
+  templateUrl: './features-section.html',
+  styleUrl: './features-section.css'
+})
+export class FeaturesSection {
+
+}
