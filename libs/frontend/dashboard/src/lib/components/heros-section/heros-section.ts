e82c45f220f2cfc9160f6075c5b45da7dd490819
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-07-04 21:04:40 +0530

    chore: Added Router Outlet

--- libs/frontend/dashboard/src/lib/components/heros-section/heros-section.ts
+++ libs/frontend/dashboard/src/lib/components/heros-section/heros-section.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'lib-dashboard-heros-section',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './heros-section.html',
   styleUrl: './heros-section.css'
 })
